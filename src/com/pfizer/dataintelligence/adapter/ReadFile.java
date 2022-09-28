cf78b3f64bb02078d50235953353cc6cc6bca2dd
pusnayek <50030193+pusnayek@users.noreply.github.com>
2022-09-28 15:16:48 +0530

    Update ReadFile.java

--- src/com/pfizer/dataintelligence/adapter/ReadFile.java
+++ src/com/pfizer/dataintelligence/adapter/ReadFile.java
@@ -9,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import com.pfizer.dataintelligence.domain.CSVEntry;
@@ -42,19 +43,27 @@ public class ReadFile {
 		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)) { 
 			// read the first line from the text file 
 			String headerLine = br.readLine(); 
-			List<String> headers = (headerLine != null) ? Arrays.asList(headerLine.split(",")) : null;
-			if(headers == null) {
+			headerLine = headerLine.replaceAll("/[^0-9A-Za-z,/_]/g", "");
+			List<String> headerStrings = (headerLine != null) ? Arrays.asList(headerLine.split(",")) : null;
+			if(headerStrings == null) {
 				System.err.println("No entry in file > " + file.getAbsolutePath());
 				return entries;
+			}
+			
+			List<String> headers = new ArrayList<>();
+			for (Iterator iterator = headerStrings.iterator(); iterator.hasNext();) {
+				String string = (String) iterator.next();
+				headers.add(string.trim());
 			}
 			
 			// loop until all lines are read 
 			String line = br.readLine(); 
 			while (line != null) { 
 				lineNum++;
+				//System.out.print("Reading line .." + lineNum);
 				// use string.split to load a string array with the values from 
 				// each line of // the file, using a comma as the delimiter 
-				String[] attributes = line.split(","); 
+				String[] attributes = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1); 
 				// adding Entry into ArrayList 
 				entries.add(new CSVEntry(headers, Arrays.asList(attributes)));  
 				// if end of file reached, line would be null 
