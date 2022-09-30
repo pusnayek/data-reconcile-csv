d6a8bb27ce6082e9fa8dbf2f54af79dfc979aab6
pusnayek <50030193+pusnayek@users.noreply.github.com>
2022-09-30 21:26:08 +0530

    Update ConfigBean.java

--- src/com/pfizer/dataintelligence/config/ConfigBean.java
+++ src/com/pfizer/dataintelligence/config/ConfigBean.java
@@ -20,23 +20,33 @@ public class ConfigBean {
 	static final String targetDirRelativePath_ = "TARGET_DIR";
 	
 	static final String dateColumns_ = "DATE_COLS";
+	static final String timeColumns_ = "TIME_COLS";
 	static final String srcDateFormat_ = "SRC_DATE_FORMAT";
 	static final String trgDateFormat_ = "TRG_DATE_FORMAT";
+	static final String srcTimeFormat_ = "SRC_TIME_FORMAT";
+	static final String trgTimeFormat_ = "TRG_TIME_FORMAT";
 	
 	public static final Pattern numberPattern = Pattern.compile("-?\\d+(\\.\\d+)?");	
 	public static final NumberFormat numberFormat = NumberFormat.getInstance(Locale.getDefault());
+	public static final SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm:ss");;
+	public static final SimpleDateFormat timeFormatterLong = new SimpleDateFormat("HH:mm:ss.SSS");;
 	
 	public List<String> ignoreColumns;
 	public List<String> keyColumns;
 	public List<String> updateColumns;
 	public List<String> dateColumns;
+	public List<String> timeColumns;
 	public String sourceDirRelativePath;
 	public String targetDirRelativePath;
 	public String srcDateFormat;
 	public String trgDateFormat;
+	public String srcTimeFormat;
+	public String trgTimeFormat;
 	
 	public SimpleDateFormat srcDateFormatter;
 	public SimpleDateFormat trgDateFormatter;
+	public SimpleDateFormat srcTimeFormatter;
+	public SimpleDateFormat trgTimeFormatter;
 
 	private static ConfigBean INSTANCE = new ConfigBean();
 
@@ -63,12 +73,17 @@ public class ConfigBean {
 		INSTANCE.keyColumns = Arrays.asList(props.getProperty(keyColumns_).split(","));
 		INSTANCE.updateColumns = Arrays.asList(props.getProperty(updateColumns_).split(","));
 		INSTANCE.dateColumns = Arrays.asList(props.getProperty(dateColumns_).split(","));
+		INSTANCE.timeColumns = Arrays.asList(props.getProperty(timeColumns_).split(","));
 		INSTANCE.sourceDirRelativePath = props.getProperty(sourceDirRelativePath_);
 		INSTANCE.targetDirRelativePath = props.getProperty(targetDirRelativePath_);
 		INSTANCE.srcDateFormat = props.getProperty(srcDateFormat_);
 		INSTANCE.trgDateFormat = props.getProperty(trgDateFormat_);
+		INSTANCE.srcTimeFormat = props.getProperty(srcTimeFormat_);
+		INSTANCE.trgTimeFormat = props.getProperty(trgTimeFormat_);
 		INSTANCE.srcDateFormatter = new SimpleDateFormat(INSTANCE.srcDateFormat);
 		INSTANCE.trgDateFormatter = new SimpleDateFormat(INSTANCE.trgDateFormat);
+		INSTANCE.srcTimeFormatter = new SimpleDateFormat(INSTANCE.srcTimeFormat);
+		INSTANCE.trgTimeFormatter = new SimpleDateFormat(INSTANCE.trgTimeFormat);
 		propertiesLoaded = true;
 		
 		System.out.println("-------------------------------------------------------");		
@@ -77,7 +92,6 @@ public class ConfigBean {
 		System.out.println("Update Columns > " + props.getProperty(updateColumns_));
 		System.out.println("Source Dir Relative Path > " + props.getProperty(sourceDirRelativePath_));
 		System.out.println("Target Dir Relative Path > " + props.getProperty(targetDirRelativePath_));
-		System.out.println("-------------------------------------------------------");		
 		
 	}
 	
